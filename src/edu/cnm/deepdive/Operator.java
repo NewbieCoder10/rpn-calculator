13a64e36db32faf6fec00e0d532240eb1f04e1e9
Khasiano <NewbieCoder10>
2019-09-19 10:49:18 -0500

    Generate token pattern dynamically.

--- src/edu/cnm/deepdive/Operator.java
+++ src/edu/cnm/deepdive/Operator.java
@@ -18,11 +18,17 @@ public enum Operator {
   /** Pops 2 values from stack, pushes quotient (real) of the 2 back onto stack. */
   DIVIDE("/"),
   /** Pops 1 value from stack, pushes its square root back onto stack. */
-  SQUARE_ROOT("sqrt"),
+  SQUARE_ROOT("sqrt") {
+    @Override
+    protected boolean needsEscape() {
+      return false;
+    }
+  },
   /** Pops 2 values from stack, pushes the value of 1st raised to the second back onto stack. */
   POWER("^"),
   /** Pops 2 values from stack, pushes remainder after truncated division of the 2 back onto stack. */
   MODULO("%");
+
 
   private String token;
 
@@ -35,8 +41,21 @@ public enum Operator {
     return token;
   }
 
+  protected  boolean needsEscape() {
+    return true;
+  }
+
   public static String tokenPattern() {
-    return "(?<=^|\\s)(\\+|\\-|\\*|\\/|\\^|\\%|sqrt)(?=\\s|$)";
+    String pattern = "";
+    for (Operator op : values()) {
+      if (op.needsEscape()) {
+        pattern+= "\\";
+      }
+      pattern += op.token + "|";
+    }
+    return String.format(
+        "(?<=^|\\s)%s(?=\\s|$)",
+        pattern.substring(0, pattern.length() -1));
   }
 
   public static void operate(String token, Deque<Double> operands) {
